4cab179e2e0bc62b872a31310aca24ec1fb813e8
Jules Stähli <jules.stahli@gmail.com>
2021-12-06 11:41:58 +0100

    Add resouces

---
+++ planet/planet.c
@@ -0,0 +1,7 @@
+#include "planet.h"
+#include <stdlib.h>
+
+#define G 6.67e-11
+#define M_SOLEIL 1.989e30
+
+// TODO : magic
---
+++ vec2/vec2.c
@@ -0,0 +1,1 @@
+// TODO : Replace this file by your vec2.c from last tp.
